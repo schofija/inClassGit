9882eb57ab30268b6ebe5a911873555005f041a6
schofija <72167596+schofija@users.noreply.github.com>
2021-04-15 16:53:12 -0700

    list

--- calc.py
+++ calc.py
@@ -1,8 +1,10 @@
 def calc(a, b):
     sum = a + b 
-    difference = a - b
-    multiply = a * b
-    divide = a / b
+    diff = a - b
+    mul = a * b
+    div = a / b
     #print(sum)
+    
+    sol = [sum, diff, mul, div]
     
 calc(3,4)
